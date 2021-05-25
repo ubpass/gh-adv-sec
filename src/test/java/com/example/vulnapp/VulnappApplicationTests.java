45cbabfd1c9c7d219136262d8ddeb926cda2da25
Jeandre Sutil <jsutil@tyro.com>
2021-05-25 16:49:14 +1000

    Initial import

---
+++ src/test/java/com/example/vulnapp/VulnappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vulnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
